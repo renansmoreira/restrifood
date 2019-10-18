7e3edc46f45f7f67a647d9ea54285168218fc527
thg.vieira12 <thg.vieira12@gma>
2019-10-18 17:15:56 -0400

    busca de fornecedores

--- app/src/Fornecedor.js
+++ app/src/Fornecedor.js
@@ -1,6 +1,4 @@
 import React from 'react';
-
-import { Link } from "react-router-dom";
 import Api from './api';
 
 class Fornecedor extends React.PureComponent {
@@ -8,32 +6,35 @@ class Fornecedor extends React.PureComponent {
   constructor(props) {
     super(props);
     this.api = new Api();
-    this.fornecedores = [{id:1, nome:"Casa do Luis", logo: ""}];
+    this.state = { fornecedores: []};
     this.api.buscarFornecedores()
-      .then((dados) => {
-        this.fornecedores = dados});
+    .then((dados) => {
+        this.setState({
+          fornecedores: dados.data.data
+        })
+    });
+      
   }
 
   irParaDetalhesDoFornecedor(fornecedor) {
     localStorage.setItem('fornecedor', fornecedor.id);
-    document.location.href = '/detalhes-fornecedor';
+    document.location.href = '/detalhes';
   }
 
   render() {
     return (
+      
       <div className="Fornecedor">
         <input type="search" />
         <span>Locais</span>
         <ul>
-          {this.fornecedores.map(fornecedor => (
-            
-            <li>
+          {this.state.fornecedores.map(fornecedor => ( 
+            <li key={fornecedor.id}>
               <div onClick={() => this.irParaDetalhesDoFornecedor(fornecedor)}>
-                <img src="{fornecedor.logo}"></img>
+                <img src={fornecedor.logo}></img>
                 <span>{fornecedor.nome}</span>
               </div>
             </li>
-            
           ))}
         </ul>
       </div>
--- app/src/api.js
+++ app/src/api.js
@@ -8,6 +8,9 @@ class Api {
   };
 
   buscarFornecedores = () => {
+    // return Promise.resolve([
+    //   {id:1, nome:"Casa do Luis", logo: 'https://uploads.knightlab.com/storymapjs/7a64bb0361468cdfc9b5bda65d5fc8f9/roteiro-burguer-cult/_images/burguer.png'}, {id:2, nome:"Casa do Bolos de Jesus", logo: ""}, {id:3, nome:"Comida Fit da Tia Maria", logo: ""}
+    // ]);
     return axios.get(`${this.ENDERECO_DA_API}fornecedores`);
   };
 
