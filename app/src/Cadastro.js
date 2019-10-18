455b1edabfba8bf0af0e48ae6c13a865d263386d
Fabio Souza <fabio@fabiowillian.com.br>
2019-10-18 14:26:04 -0400

    Finalizado componente de cadastro

--- app/src/Cadastro.js
+++ app/src/Cadastro.js
@@ -1,50 +1,73 @@
 import React from 'react';
 
 import { Link } from "react-router-dom";
+import Api from './api';
+
+import { useHistory } from "react-router-dom";
 
 class Cadastro extends React.PureComponent {
-  
+
   constructor(props) {
     super(props);
     this.state = {
+      nome: '',
+      email: '',
+      telefone: '',
+      senha: '',
+      confirmacaoDeSenha: ''
     };
 
+    this.api = new Api();
+    this.history = useHistory();
+
     this.alterarNome = this.alterarNome.bind(this);
+    this.alterarEmail = this.alterarEmail.bind(this);
+    this.alterarTelefone = this.alterarTelefone.bind(this);
+    this.alterarSenha = this.alterarSenha.bind(this);
+    this.alterarConfirmacaoDeSenha = this.alterarConfirmacaoDeSenha.bind(this);
+    this.salvarInformacoesDoUsuario = this.salvarInformacoesDoUsuario.bind(this);
   }
 
   alterarNome(evento) {
     this.setState({
       ...this.state,
-      Nome: evento.target.value
+      nome: evento.target.value
     })
   }
 
   alterarEmail(evento) {
     this.setState({
       ...this.state,
-      Email: evento.target.value
+      email: evento.target.value
     })
   }
 
   alterarTelefone(evento) {
     this.setState({
       ...this.state,
-      Telefone: evento.target.value
+      telefone: evento.target.value
     })
   }
 
   alterarSenha(evento) {
     this.setState({
       ...this.state,
-      Senha: evento.target.value
+      senha: evento.target.value
     })
   }
 
   alterarConfirmacaoDeSenha(evento) {
     this.setState({
       ...this.state,
-      ConfirmacaoDeSenha: evento.target.value
+      confirmacaoDeSenha: evento.target.value
     })
+  }
+
+  salvarInformacoesDoUsuario() {
+    const usuario = {...this.state};
+    console.log('Usuário', usuario);
+    localStorage.setItem('restrifood-usuario', usuario);
+    this.history.push('/restricoes');
   }
 
   render() {
@@ -56,30 +79,28 @@ class Cadastro extends React.PureComponent {
 
         <form>
           <label>Seu nome</label><br />
-          <input type="text" value={this.state.Nome} 
+          <input type="text" value={this.state.Nome}
             onChange={this.alterarNome} /><br />
 
-          Meu nome: <p>{this.state.Nome}</p>
-
           <label>E-mail</label><br />
-          <input type="email" value={this.state.Email} 
+          <input type="email" value={this.state.Email}
             onChange={this.alterarEmail} /><br />
 
           <label>Telefone</label><br />
-          <input type="text" value={this.state.Telefone} 
-            onChange={this.alterarTelefone}/><br />
+          <input type="text" value={this.state.Telefone}
+            onChange={this.alterarTelefone} /><br />
 
           <label>Senha</label><br />
-          <input type="password" value={this.state.Senha} 
-            onChange={this.alterarSenha}/><br />
+          <input type="password" value={this.state.Senha}
+            onChange={this.alterarSenha} /><br />
 
           <label>Confirme sua senha</label><br />
-          <input type="password" value={this.state.ConfirmacaoDeSenha} 
+          <input type="password" value={this.state.ConfirmacaoDeSenha}
             onChange={this.alterarConfirmacaoDeSenha} /><br />
 
-          <button type="submit" className="botao">
+          <button type="button" className="botao" onClick={this.salvarInformacoesDoUsuario}>
             Criar minha conta
-        </button><br />
+          </button><br />
 
           <Link to="/">Prefiro criar depois</Link>
         </form>
