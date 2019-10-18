5e5a43e765eb2bd642b6efb58f5b454470fca6e9
Renan S Moreira <renan.siravegna@gmail.com>
2019-10-18 15:21:57 -0400

    Adicionando patologias e já modelando uma ideia de fornecedores e produtos

--- api/src/clientes/models.py
+++ api/src/clientes/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+import enum
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=500)
     telefone = models.CharField(max_length=15)
     email = models.CharField(max_length=150)
     senha = models.CharField(max_length=100)
+    patologias = models.CharField(max_length=500, null=True)
 
     @classmethod
     def criar(self, **kwargs):
@@ -33,3 +35,23 @@ class Cliente(models.Model):
             raise Exception('As senhas digitadas não são iguais')
 
         return self(nome=nome, telefone=telefone, email=email, senha=senha)
+
+    def obter_patologias(self):
+        if not self.patologias:
+            return []
+
+        return self.patologias.split(',')
+
+    def adicionar_patologias(self, patologias):
+        separador = ','
+        self.patologias = separador.join(patologias)
+
+class Patologia(enum.Enum):
+    RENAL_CRONICA = 'Renal crônica'
+    DIABETES = 'Diabetes'
+    HIPERTENSAO = 'Hipertensão'
+    INTOLERANCIA_A_GLUTEN = 'Intolerância a glúten'
+    INTOLERANCIA_A_LACTOSE = 'Intolerancia a lactose'
+    GASTRITE = 'Gastrite'
+    CALCULO_RENAL = 'Cálculo renal'
+    OBESIDADE = 'Obesidade'
