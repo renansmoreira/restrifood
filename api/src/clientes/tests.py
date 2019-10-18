78c07b00fad7e8c285b175b67b761f6082b78195
Renan S Moreira <renan.siravegna@gmail.com>
2019-10-18 13:43:10 -0400

    Criando um modelo de cliente

--- api/src/clientes/tests.py
+++ api/src/clientes/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from clientes.models import Cliente
 
-# Create your tests here.
+class ClienteTest(TestCase):
+
+    def test_deve_validar_se_a_confirmacao_de_senha_esta_correta_na_criacao(self):
+        try:
+            Cliente.criar(nome='Renan', telefone='99999-1234', email='email@email.com',
+                senha='senha123', confirmacao_de_senha='senha diferente')
+        except Exception as exception:
+            self.assertEqual('As senhas digitadas não são iguais', str(exception))
+            return
+
+        self.fail('Exceção esperada, mas nenhuma lançada')
