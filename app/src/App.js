bbdf2352ad12d4726ec96c945f0189595a2019c5
Jamal Charanek <askookbox@gmail.com>
2019-10-18 16:52:55 -0400

    Trocando logo

--- app/src/App.js
+++ app/src/App.js
@@ -8,9 +8,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Link to="/sobre">
-          <h2>Restrifood</h2>
+          <img src={logo} className="App-logo" alt="logo" />
         </Link>
       </header>
     </div>
