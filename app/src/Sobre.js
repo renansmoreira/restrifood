6162f0b6bc673356f5a750725d15a5103ea3d276
Fabio Souza <fabio@fabiowillian.com.br>
2019-10-18 14:03:50 -0400

    Criado componente de cadastro

--- app/src/Sobre.js
+++ app/src/Sobre.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 
+import { Link } from "react-router-dom";
+
 function Sobre() {
   return (
     <div className="Sobre">
       <header className="App-header">
         <h2>Restrifood</h2>
         <img src={logo} className="App-logo" alt="logo" />
-
+        
         <p>
           O Restrifood nasceu da necessidade de atender pessoas com restrições alimentares e condições
           de saúde que requerem maior atenção ao que se come.
         </p>
+
+        <Link to="/cadastro" className="botao">Avançar ></Link>
       </header>
     </div>
   );
